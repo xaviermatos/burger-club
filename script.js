a3a76d15fe75c97ba47df672ae63b2ed17967867
xaviermatos <xmatos@aoiths.org>
2017-06-30 18:12:15 +0000

    Added final features

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ $(document).ready(function() {
 $(".btn").click(function(){
     var name = $(".form").val();
     var email = $(".form2").val();
-    $('.modal-content').on('shown.bs.modal', function () {
-   var modal = $(this)
-   modal.find('.modal').text("thank you " + name + " for signing up! We will send an email to " + email +" shortly");
+    $('#myModal').on('shown.bs.modal', function () {
+    $('.modal-body').text("Thank you " + name + " for signing up! We will send an email to " + email +" shortly");
 })
     //alert("thank you " + name + " for signing up! We will send an email to " + email +" shortly");
 });
