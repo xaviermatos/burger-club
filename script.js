90793c0888c7216e291ea85ee7d16135872513bb
xaviermatos <xmatos@aoiths.org>
2017-06-30 15:27:08 +0000

    added maps and sending mail

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
 $(document).ready(function() {
 
 $(".btn").click(function(){
-    var name = $(".form-control").val();
-    var email = $(".form-controle").val();
-    alert("thank you " + name + " for signing up! We will send an email to " + email +" shortly");
+    var name = $(".form").val();
+    var email = $(".form2").val();
+    $('.modal-content').on('shown.bs.modal', function () {
+   var modal = $(this)
+   modal.find('.modal').text("thank you " + name + " for signing up! We will send an email to " + email +" shortly");
+})
+    //alert("thank you " + name + " for signing up! We will send an email to " + email +" shortly");
 });
 
-$("#f").hover(function() {
-    $("#f").toggle( "bounce", { times: 3 }, "slow" ); 
+$('#f').mouseenter(function() {
+    $( this ).css("opacity" , 0.5)
   });
 
+$('#f').mouseleave(function() {
+    $(this).css("opacity",1);
+  });
+  
+  $('#t').mouseenter(function() {
+    $(this).css("opacity",0.5);
+  });
+
+$('#t').mouseleave(function() {
+    $(this).css("opacity",1);
+  });
+  
+  $('#i').mouseenter(function() {
+    $(this).css("opacity",0.5);
+  });
+
+$('#i').mouseleave(function() {
+    $(this).css("opacity",1);
+  });
+  
+  $('#l').mouseenter(function() {
+    $(this).css("opacity",0.5);
+  });
+
+$('#l').mouseleave(function() {
+    $(this).css("opacity",1);
+  });
 });
 
     
